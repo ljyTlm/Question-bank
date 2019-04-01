0a233992eed1bb50f3e23a4b45ad0e508d95d3d1
ljyTlm <970955947@qq.com>
2019-04-02 00:24:31 +0800

    哈哈哈哈哈哈 竟然一遍ac了 我的天 真的ac了 知识点：累加和 = 一个多项式 这个多项式的常量是随着n固定的 然后 直接暴力字典序全排列 判断over

--- Acm/src/洛谷oj/数字三角形_P1118.java
+++ Acm/src/洛谷oj/数字三角形_P1118.java
@@ -1,29 +1,74 @@
 package Âå¹Èoj;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 
 
 public class Êý×ÖÈý½ÇÐÎ_P1118 {
 
+	private static int[] arr;
+	private static int m;
+	private static int n;
+	private static int[][] vla;
+	private static boolean flag;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		while (sc.hasNext()) {
-			int n = sc.nextInt();
-			HashMap<String, Integer> [][] arr = new HashMap [n][n];
-			for (int i = 0; i < arr.length; i++) {
-				arr[0][i] = new HashMap<>();
-				arr[0][i].put("a"+i, 1);
+		vla = new int [13][13];
+		vla[0][0] = 1;
+		for (int i = 1; i < vla.length; i++) {
+			vla[i][0] = 1;
+			for (int j = 1; j <= i; j++) {
+				vla[i][j] = vla[i-1][j-1] + vla[i-1][j];
 			}
-			for (int i = 1; i < arr.length; i++) {
-				for (int j = 0; j < arr.length-i; j++) {
-					arr[i][j] = new HashMap<>();
-					for (String key : arr[i-1][j].keySet()) {
-//						Integer v = arr[i][j] 
+		}
+		while (sc.hasNext()) {
+			n = sc.nextInt();
+			m = sc.nextInt();
+			arr = new int [n];
+			for (int i = 0; i < arr.length; i++) {
+				arr[i] = i+1;
+			}
+			flag = false;
+			quick(0);
+		}
+	}
+
+	private static void quick(int s) {
+		// TODO Auto-generated method stub
+		if (flag) {
+			return;
+		}
+		if (s >= arr.length) {
+			int sum = 0;
+			for (int i = 0; i < arr.length; i++) {
+				sum += arr[i]*vla[n-1][i];
+			}
+			if (sum == m) {
+				for (int i = 0; i < arr.length; i++) {
+					if (i == 0) {
+						System.out.print(arr[i]);
+					}else {
+						System.out.print(" "+arr[i]);
 					}
 				}
+				flag = true;
 			}
+			return;
+		}
+		for (int i = s; i < arr.length; i++) {
+			int flag = arr[i];
+			for (int j = i; j > s; j--) {
+				arr[j] = arr[j-1];
+			}
+			arr[s] = flag;
+			quick(s+1);
+			for (int j = s; j < i; j++) {
+				arr[j] = arr[j+1];
+			}
+			arr[i] = flag;
 		}
 	}
 
