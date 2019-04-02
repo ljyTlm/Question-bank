c51e6d6be90677394caed9e3469e669f5914f2d8
ljyTlm <970955947@qq.com>
2019-04-02 23:11:52 +0800

    stl大法好 next_permutation生成下一个排列 prev_permutation生成上一个排列 还有一种神仙解法 说是用康托展开 变成进制数 然后再解 虽不明但觉厉

---
+++ Acm/src/洛谷oj/火星人_P1088.java
@@ -0,0 +1,45 @@
+package Âå¹Èoj;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class »ðÐÇÈË_P1088 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+//#include<stdio.h>
+//#include<string.h>
+//#include<stdlib.h>
+//#include<queue>
+//#include<math.h>
+//#include<vector>
+//#include<map>
+//#include<set>
+//#include<stdlib.h>
+//#include<cmath>
+//#include<string>
+//#include<algorithm>
+//#include<iostream>
+//#include<map>
+//#include<stack>
+//#define LL long long 
+//using namespace std;
+//
+//int n, m, val[10001]; 
+//
+//int main()
+//{ 
+//	cin>>n>>m;
+//	for(int i = 0; i < n; i ++)
+//		cin>>val[i];
+//	while(m -- > 0)
+//		next_permutation(val, val+n);
+//	for(int i = 0; i < n-1; i ++)
+//		cout<<val[i]<<" ";
+//	cout<<val[n-1]<<endl;
+//    return 0;
+//}
