88e4e584b47eb51937a056f70a37429451960520
ljyTlm <970955947@qq.com>
2019-04-09 21:34:41 +0800

    能正常写 就别画蛇添足 又一道四维dp题~

--- Acm/src/洛谷oj/乌龟棋_P1541.java
+++ Acm/src/洛谷oj/乌龟棋_P1541.java
@@ -13,8 +13,7 @@ public class ÎÚ¹êÆå_P1541 {
 		int n = sc.nextInt();
 		int m = sc.nextInt();
 		int [] arr = new int[n+1];
-		for (int i = 1; i < arr
-				.length; i++) {
+		for (int i = 1; i < arr.length; i++) {
 			arr[i] = sc.nextInt()+1;
 		}
 		int [] num = new int[5];
@@ -29,24 +28,18 @@ public class ÎÚ¹êÆå_P1541 {
 					for (int z = 0; z <= num[4]; z++) {
 						if (dp[i][j][k][z] > 0) {
 							if(i < num[1])
-								dp[i+1][j][k][z] = Math.max(dp[i+1][j][k][z], dp[i][j][k][z]+arr[1+i+j+k+z+1]);   
+								dp[i+1][j][k][z] = Math.max(dp[i+1][j][k][z], dp[i][j][k][z]+arr[1+i+j*2+k*3+z*4+1]);   
 							if(j < num[2])
-								dp[i][j+1][k][z] = Math.max(dp[i][j+1][k][z], dp[i][j][k][z]+arr[1+i+j+k+z+2]);
+								dp[i][j+1][k][z] = Math.max(dp[i][j+1][k][z], dp[i][j][k][z]+arr[1+i+j*2+k*3+z*4+2]);
 							if(k < num[3])
-								dp[i][j][k+1][z] = Math.max(dp[i][j][k+1][z], dp[i][j][k][z]+arr[1+i+j+k+z+3]);
+								dp[i][j][k+1][z] = Math.max(dp[i][j][k+1][z], dp[i][j][k][z]+arr[1+i+j*2+k*3+z*4+3]);
 							if(z < num[4])
-								dp[i][j][k][z+1] = Math.max(dp[i][j][k][z+1], dp[i][j][k][z]+arr[1+i+j+k+z+4]);
+								dp[i][j][k][z+1] = Math.max(dp[i][j][k][z+1], dp[i][j][k][z]+arr[1+i+j*2+k*3+z*4+4]);
 						}
 					}
 				}
 			}
 		}
 		System.out.println(dp[num[1]][num[2]][num[3]][num[4]]-m-1);
-	}
-	static class Node {
-		public Node(int a, int b, int c, int d, int v, int idx) {
-			this.a = a; this.b = b; this.c = c; this.d = d; this.v = v; this.idx = idx;
-		}
-		int a, b, c, d, v, idx;
 	}
 }
