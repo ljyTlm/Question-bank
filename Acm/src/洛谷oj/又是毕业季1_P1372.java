29f9f0e20742ee60c9f6ca1d8cdc9af7dccf5ede
ljyTlm <970955947@qq.com>
2019-04-12 11:15:09 +0800

    这题看了题解 哈哈 数学题 不看题解是做不出来啊

---
+++ Acm/src/洛谷oj/又是毕业季1_P1372.java
@@ -0,0 +1,15 @@
+package ¬Âπ»oj;
+
+import java.util.Scanner;
+
+public class ”÷ «±œ“µºæ1_P1372 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		System.out.println(n/k);
+	}
+
+}
