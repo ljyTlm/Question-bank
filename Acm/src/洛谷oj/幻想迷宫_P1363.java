d91034f613b951c6022baacddabbb440be2e64f7
ljyTlm <970955947@qq.com>
2019-04-15 01:03:51 +0800

    拿下这道题可真不容易 我的思路是没有毛病的 但是有如下问题 1.考虑问题不全面 2.拘泥于板子 和经验 我们需要这样一个标记 既能让走过的路不在走 并且还可以在另外一个图里到达此点 所以我们直接标记xy就行 用的时候做查 取最大值 如果大于1那么表明他是从另外一个图来的 如果等于1表示是同一张图相同点来的 这样就巧妙的符合了我们的需求

--- Acm/src/洛谷oj/幻想迷宫_P1363.java
+++ Acm/src/洛谷oj/幻想迷宫_P1363.java
@@ -34,36 +34,108 @@ public class ª√œÎ√‘π¨_P1363 {
 					}
 				}
 			}
-			int [][][] mp = new int[8][n][m];
+			int [][] mp = new int[3*n][3*m];
+			mp[s.i][s.j] = 1;
 			LinkedList<Node> qu = new LinkedList<Node>();
-			int flag = 0;
+			boolean flag = true;
 			qu.add(s);
-			while (!qu.isEmpty()) {
+			while (!qu.isEmpty() && flag) {
 				Node node = qu.poll();
-				if(flag != 0) {
-					if(node.i%n == x && node.j%m == y) {
-						flag = -1;
-						break;
-					}
-				}
-				flag ++;
 				for (int i = 0; i < pla.length; i+=2) {
 					Node next = new Node();
 					next.i = node.i + pla[i];
 					next.j = node.j + pla[i+1];
-					if(next.i >= 0 && next.i < 3*n && next.j >= 0 && next.j < 3*m && arr[next.i%n][next.j%m] == '.') {
-						String str = next.toString();
-//						if (mp[][][] == 0) {
-//							mp[][][] = ;
-//							qu.add(next);
-//						}
+					if(next.i >= 0 && next.i < 3*n && next.j >= 0 && next.j < 3*m && arr[next.i % n][next.j % m] == '.') {
+						if (mp[next.i][next.j] == 0) {
+							mp[next.i][next.j] = 1;
+							qu.add(next);
+							//System.out.println(next.i+" "+next.j);
+							if (next.i % n == x && next.j % m == y) {
+								flag = false;
+								break;
+							}
+						}
 					}
 				}
 			}
-			System.out.println(flag == -1 ? "Yes":"No");
+			System.out.println(flag? "No":"Yes");
 		}
 	}
 	static class Node {
 		int i, j;
 	}
 }
+//#include<iostream>  
+//#include<cstdio>  
+//#include<cmath> 
+//#include <algorithm> 
+//#include <map>
+//#include <queue>
+//#include <string>
+//#include <sstream>
+//
+//using namespace std;   
+//
+//
+//struct Node {
+//	int i, j;
+//};
+//
+//int n, m, x, y, a, b; 
+//char arr[1510][1510];
+//int flag[1510][1510][2];
+//Node s, p, node;
+//int pla[8] = {1, 0, -1, 0, 0, 1, 0, -1};
+//queue<Node>	qu;
+//string next;
+//
+//int main()  
+//{   
+//	while(scanf("%d%d", &n, &m) != EOF){
+//		for(int i = 0; i < n; i ++){
+//			for(int j = 0; j < m; j ++){
+//				cin>>arr[i][j];
+//				if(arr[i][j] == 'S'){
+//					x = i;
+//					y = j;
+//					arr[i][j] = '.';
+//				}
+//				flag[i][j][0] = flag[i][j][1] = -1;
+//			} 
+//		}	
+//		if(n == 1 || m == 1){
+//			cout<<"Yes"<<endl;
+//			continue;
+//		}
+//		s.i = x + 999*n;
+//		s.j = y + 999*m; 
+//		flag[x][y][0] = s.i;
+//		flag[x][y][1] = s.j;
+//		qu.push(s);
+//		bool k = true;
+//		while(qu.size() != 0 && k){
+//			node = qu.front(); qu.pop();
+//	 		for(int i = 0; i < 8; i += 2){
+//		 		p.i= node.i+pla[i];
+//	 		 	p.j= node.j+pla[i+1];
+//		 		a = p.i%n;
+//		 		b = p.j%m;
+//		 		if(arr[a][b] == '.'){
+//		 			if(flag[a][b][0] == -1){
+//			 			flag[a][b][0] = p.i;
+//						flag[a][b][1] = p.j;
+//						qu.push(p);	
+//			 		}else{
+//		 				if(max(abs(p.i-flag[a][b][0]), abs(p.j-flag[a][b][1])) > 1){
+//				 			k = false;
+//				 			break;
+//				 		}
+//		 			}
+//		 		}
+//		 	}
+//		}
+//		cout<< (k ? "No":"Yes") <<endl;
+//		while(qu.size() != 0) qu.pop();
+//	}       	
+//    return 0;  
+//}
