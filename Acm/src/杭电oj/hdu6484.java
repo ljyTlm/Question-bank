7e4eee5575a839761cc85e9885de3e11e126b567
ljyTlm <970955947@qq.com>
2019-04-16 17:07:43 +0800

    第一次创建txt文件 用来记录刷题中遇到的 数学公式和定理

--- Acm/src/杭电oj/hdu6484.java
+++ Acm/src/杭电oj/hdu6484.java
@@ -1,10 +1,22 @@
 package º¼µçoj;
 
+import java.util.Scanner;
+
 public class hdu6484 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while (t -- > 0) {
+			int n = sc.nextInt();
+			int m = sc.nextInt();
+			if (n%m==0) {
+				System.out.println(0);
+			}else {
+				System.out.println(1);
+			}
+		}
 	}
 
 }
