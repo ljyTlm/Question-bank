ab530b61754c9f4ec8b8d60e293acdd5590ff405
ljyTlm <970955947@qq.com>
2019-04-17 09:07:56 +0800

    每个骨牌长度至少为1 距离最大的骨牌用手推 能推k次 所以排除前k-1个大的数 累加的和就是最小值

---
+++ Acm/src/杭电oj/hdu5777.java
@@ -0,0 +1,55 @@
+package º¼µçoj;
+
+import java.util.Scanner;
+
+public class hdu5777 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while (t-- > 0) {
+			int n = sc.nextInt();
+			int k = sc.nextInt();
+		}
+	}
+
+}
+//#include<stdio.h>
+//#include<string.h>
+//#include<stdlib.h>
+//#include<queue>
+//#include<math.h>
+//#include<vector>
+//#include<map>
+//#include<set>
+//#include<stdlib.h>
+//#include<cmath>
+//#include<string>
+//#include<algorithm>
+//#include<iostream>
+//#include<map>
+//#include<stack> 
+//#include<sstream>
+//#define LL long long
+//using namespace std;
+//
+//int t, n, k;
+//LL arr[100003], ans; 
+//
+//int main()
+//{
+//	cin>>t;
+//	while(t-->0){
+//		cin>>n>>k;		
+//		for(int i = 0; i < n - 1; i ++){
+//			scanf("%lld", &arr[i]);
+//		}
+//		sort(arr, arr+n-1);
+//		ans = n;
+//		for(int i = 0; i < n - k; i ++)
+//			ans += arr[i];
+//		cout<<ans<<endl;		
+//	}	
+//    return 0;
+//}
