e76b2db775318e7b2e159e4953b64e437fd4c69b
ljyTlm <970955947@qq.com>
2019-04-18 23:17:06 +0800

    2017年的题 利用奇偶性考虑问题

---
+++ Acm/src/杭电oj/hdu6235.java
@@ -0,0 +1,54 @@
+package º¼µçoj;
+
+public class hdu6235 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+//#include<iostream>  
+//#include<cstdio>  
+//#include<cmath> 
+//#include <algorithm> 
+//#include <map>
+//#include <queue>
+//#include <string>
+//#include <sstream>
+//#include <string.h>
+//#define LL long long
+//
+//using namespace std;   
+//
+//int t, n;
+//
+//int main()  
+//{   
+//	cin>>t;
+//	while(t-->0){
+//		cin>>n;
+//		cout<<1;
+//		if(n&1){
+//			int a = 2;
+//			int b = n/2+2;
+//			while(a <= n/2+1 || b <= n){
+//				if(b <= n)
+//					cout<<" "<<(b++); 
+//				if(a <= n/2+1)
+//					cout<<" "<<(a++); 
+//			}
+//		}else{
+//			int a = 2;
+//			int b = n/2+1;
+//			while(a <= n/2 || b <= n){
+//				if(b <= n)
+//					cout<<" "<<(b++); 
+//				if(a <= n/2)
+//					cout<<" "<<(a++);
+//			}
+//		}
+//		cout<<"\n";
+//	}
+//    return 0;  
+//}
