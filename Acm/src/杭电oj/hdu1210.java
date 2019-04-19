6be233b31f38e7c8ecf81aeadaee0a7b1c7a6314
ljyTlm <970955947@qq.com>
2019-04-19 20:01:45 +0800

    模拟水题 我还以为是找规律

---
+++ Acm/src/杭电oj/hdu1210.java
@@ -0,0 +1,48 @@
+package º¼µçoj;
+
+public class hdu1210 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+//#include<stdio.h>
+//#include<string.h>
+//#include<stdlib.h>
+//#include<queue>
+//#include<math.h>
+//#include<vector>
+//#include<map>
+//#include<set>
+//#include<stdlib.h>
+//#include<cmath>
+//#include<string>
+//#include<algorithm>
+//#include<iostream>
+//#include<map>
+//#include<stack> 
+//#include<sstream>
+//#define LL long long
+//using namespace std;
+//
+//int n, ans, idx;
+//
+//int main()
+//{
+//	while(cin>>n){
+//		ans = 1;
+//		idx = 2*n - 1;
+//		while(idx != 2*n){
+//			if(idx > n){
+//				idx = 2*(idx - n) - 1;
+//			}else{
+//				idx *= 2;
+//			}
+//			ans ++;
+//		}
+//		cout<<ans<<endl;
+//	}	
+//    return 0;
+//}
