cba63c89b5e7f36ccc6a5bb202e6866cae8da52b
ljyTlm <970955947@qq.com>
2019-04-24 20:42:32 +0800

    hdu不支持bufferReader 转战cf

--- Acm/src/杭电oj/hdu6501.java
+++ Acm/src/杭电oj/hdu6501.java
@@ -1,75 +1,69 @@
 package 杭电oj;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.math.BigInteger;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class hdu6501 {
 	
 	static int mod = 1000000007;
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
-
-		Scanner sc = new Scanner(System.in);
-		while(sc.hasNext()) {
-			long [] arr = new long[60];
-			for (int i = 0; i < arr.length; i++) {
-				arr[i] = sc.nextLong();
+		long one = 1;
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		while (true) {
+			long [] a = new long[60];
+			long [] c = new long[60];
+			StringTokenizer sc = new StringTokenizer(reader.readLine());
+			for (int i = 0; i < a.length; i++) {
+				a[i] = Long.parseLong(sc.nextToken());
+				c[i] = one<<i;
 			}
-			long n = sc.nextLong(); 
-			long flag = 0;
-			for (int i = 0; i < n; i++) {
-				String str = sc.next();
-				if (flag == 0) {
-					String val = new BigInteger(str,10).toString(2); //转成二进制
-					int k = 0;
-					for (int j = val.length()-1; j >= 0; j--) {
-						if (val.charAt(j) == '1') {
-							long num = 1;
-							for (int j2 = k; j2 >= 0; j2--) {
-								if (num <= arr[j2]) {
-									arr[j2] -= num;
-									num = 0;
-									break;
-								}else {
-									num -= arr[j2];
-									arr[j2] = 0;
-									num *= 2;
-								}
+			sc = new StringTokenizer(reader.readLine());
+			int n = Integer.parseInt(sc.nextToken());
+			long [] b = new long[n];
+			sc = new StringTokenizer(reader.readLine());
+			for (int i = 0; i < b.length; i++) {
+				b[i] = Long.parseLong(sc.nextToken());
+			}
+			Arrays.sort(b);
+			boolean flag = true;
+			for (int i = n-1; i >= 0 && flag; i--) {
+				for (int j = 59; j >= 0; j--) {
+					if (a[j] != 0 && b[i] > c[j]) {
+						long num = b[i] / c[j];
+						if (num <= a[j]) {
+							a[j] -= num;
+							for (int k = 0; k < num; k++) {
+								b[i] -= c[j];
 							}
-							if (num != 0) {
-								flag = -1;
-								break;
+						}else {
+							for (int k = 0; k < a[j]; k++) {
+								b[i] -= c[j];
 							}
+							a[j] = 0;
 						}
-						k ++;
 					}
 				}
+				if (b[i] != 0) {
+					flag = false;
+				}
 			}
-			if (flag == -1) {
+			if (!flag) {
 				System.out.println(-1);
 				continue;
 			}
 			long ans = 0;
-			for (int i = 0; i < arr.length; i++) {
-				if (arr[i] != 0) {
-					ans += (arr[i]%mod * pow(2, i))%mod;
-					ans %= mod;
-				}
+			for (int i = 0; i < 60; i++) {
+				ans += ( (a[i] % mod) * (c[i]%mod) ) % mod;
+				ans %= mod;
 			}
 			System.out.println(ans);
 		}
-	}
-	private static long pow(long a, long b) {
-		// TODO Auto-generated method stub
-		long ans = 1;
-		while (b > 0) {
-			if (b%2 == 1) {
-				ans = (ans*a)%mod;
-			}
-			a = (a * a)%mod;
-			b /= 2;
-		}
-		return ans % mod;
+		
 	}
 
 }
