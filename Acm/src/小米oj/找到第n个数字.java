67cd14771089e8ad94a1d72945f450f05e00057b
ljyTlm <970955947@qq.com>
2019-05-03 18:40:54 +0800

    数学的应用 n*(n+1)/2

---
+++ Acm/src/小米oj/找到第n个数字.java
@@ -0,0 +1,44 @@
+package 小米oj;
+
+public class 找到第n个数字 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+//#include <iostream>
+//#include <cmath>
+//#define LL long long
+//
+//using namespace std;
+//
+//
+//LL n, idx, k, sum;
+//double ans; 
+//
+//int main()
+//{
+//	scanf("%lld", &n);
+//	ans = (sqrt(n*8+1)-1)/2;
+//	k = (LL)ans;
+//	if(ans - k == 0)
+//		k --;
+//	n -= k*(k+1)/2;
+//	sum = 1;
+//	idx = 1;
+//	while(--n>0){
+//		sum += idx;
+//		if(sum == 10){
+//			sum = 8;
+//			idx = -1;
+//		}
+//		if(sum == 0){
+//			sum = 2;
+//			idx = 1;
+//		}
+//	}
+//	printf("%lld\n", sum);
+//	return 0;
+//}
